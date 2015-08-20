7654b41714bcbda9de885763c571f845cad42dc3
Brendan StJohn <bstjohn@gilt.com>
2015-08-20 16:28:01 +0100

    Update the lambda function with the newly uploaded jar

--- src/main/scala/com/gilt/aws/lambda/AwsLambdaPlugin.scala
+++ src/main/scala/com/gilt/aws/lambda/AwsLambdaPlugin.scala
@@ -1,17 +1,18 @@
 package com.gilt.aws.lambda
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.services.lambda.AWSLambdaClient
+import com.amazonaws.services.lambda.model.UpdateFunctionCodeRequest
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.{CannedAccessControlList, PutObjectRequest}
+import com.amazonaws.services.s3.model.{PutObjectResult, CannedAccessControlList, PutObjectRequest}
 import sbt._
-
-import scala.util.control.NonFatal
 
 object AwsLambdaPlugin extends AutoPlugin {
 
   object autoImport {
-    val deployLambda = taskKey[Boolean]("Package and deploy lambda function to AWS")
+    val deployLambda = taskKey[Unit]("Package and deploy lambda function to AWS")
     val s3Bucket = settingKey[Option[String]]("S3 bucket where lambda function will be deployed to")
+    val lambdaFunctionName = settingKey[Option[String]]("Name of the lambda function to update")
   }
 
   import autoImport._
@@ -19,42 +20,69 @@ object AwsLambdaPlugin extends AutoPlugin {
   override def requires = sbtassembly.AssemblyPlugin
 
   override lazy val projectSettings = Seq(
-    deployLambda := deploy(sbtassembly.AssemblyKeys.assembly.value, s3Bucket.value),
-    s3Bucket := None
+    deployLambda := {
+      val bucketId = s3Bucket.value match {
+        case Some(id) => id
+        case None => sys.env.get("AWS_LAMBDA_BUCKET_ID") match {
+          case Some(envVarId) => envVarId
+          case None => promptUserForS3BucketId()
+        }
+      }
+
+      val functionName = lambdaFunctionName.value match {
+        case Some(f) => f
+        case None => sys.env.get("AWS_LAMBDA_FUNCTION_NAME") match {
+          case Some(envVarFunctionName) => envVarFunctionName
+          case None => promptUserForFunctionName()
+        }
+      }
+
+      val jar = sbtassembly.AssemblyKeys.assembly.value
+
+      deploy(jar, bucketId)
+      updateLambda(functionName, bucketId, jar.getName)
+    },
+    s3Bucket := None,
+    lambdaFunctionName := None
   )
 
-  private def deploy(jar: File, bucketIdOpt: Option[String]): Boolean = {
-    val amazonS3Client = new AmazonS3Client(new ProfileCredentialsProvider)
+  private def deploy(jar: File, bucketId: String): PutObjectResult = {
+    val amazonS3Client = new AmazonS3Client(credentials)
 
-    val bucketId = bucketIdOpt match {
-      case Some(id) => id
-      case None => sys.env.get("AWS_LAMBDA_BUCKET_ID") match {
-        case Some(envVarId) => envVarId
-        case None => promptUserForS3BucketId()
-      }
+    val objectRequest = new PutObjectRequest(bucketId, jar.getName, jar)
+
+    objectRequest.setCannedAcl(CannedAccessControlList.AuthenticatedRead)
+    amazonS3Client.putObject(objectRequest)
+  }
+
+  private def updateLambda(functionName: String, bucketId: String, s3Key: String) = {
+    val client = new AWSLambdaClient(credentials)
+
+    val request = {
+      val r = new UpdateFunctionCodeRequest()
+      r.setFunctionName(functionName)
+      r.setS3Bucket(bucketId)
+      r.setS3Key(s3Key)
+
+      r
     }
-    
-    try {
-      val objectRequest = new PutObjectRequest(bucketId, jar.getName, jar)
 
-      objectRequest.setCannedAcl(CannedAccessControlList.AuthenticatedRead)
-      amazonS3Client.putObject(objectRequest)
-
-      println(s"File ${jar.getName} uploaded to bucket $bucketIdOpt")
-      true
-    } catch {
-      case NonFatal(e) => {
-        println(s"Could not upload file ${jar.getCanonicalPath}: ${e.getMessage}\n${e.getStackTraceString}")
-        false
-      }
-    }
+    client.updateFunctionCode(request)
   }
 
   def promptUserForS3BucketId(): String = {
-
     SimpleReader.readLine("Enter the AWS S3 bucket where the lambda function will be stored\n") match {
       case Some(id) => id
       case None => promptUserForS3BucketId()
     }
   }
+
+  def promptUserForFunctionName(): String = {
+    SimpleReader.readLine("Enter the name of the lambda function to be executed\n") match {
+      case Some(f) => f
+      case None => promptUserForFunctionName()
+    }
+  }
+
+  lazy val credentials = new ProfileCredentialsProvider()
 }
